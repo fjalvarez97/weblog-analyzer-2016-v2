ffd12dae3c7501ede186dc5049ef08f8e366d059
fjalvarez97 <franjalvarez955@gmail.com>
2018-03-07 03:42:56 +0100

    Ahora el constructor de la clase acceso tiene un solo dato String por parametro

--- Acceso.java
+++ Acceso.java
@@ -5,36 +5,36 @@ public class Acceso
     private int dia;
     private int hora;
     private int minutos;
-    
-    public Acceso(int ano, int mes, int dia, int hora, int minutos)
+
+    public Acceso(String fecha)
     {
-        this.ano = ano;
-        this.mes = mes;
-        this.dia = dia;
-        this.hora = hora;
-        this.minutos = minutos;
+        ano = Integer.parseInt(fecha.substring(0,4));
+        mes = Integer.parseInt(fecha.substring(5,7));
+        dia = Integer.parseInt(fecha.substring(8,10));
+        hora = Integer.parseInt(fecha.substring(11,13));
+        minutos = Integer.parseInt(fecha.substring(14,16));
     }
-    
+
     public int getAno() 
     {
         return ano;
     }
-    
+
     public int getMes()
     {
         return mes;
     }
-    
+
     public int getDia()
     {
         return dia;
     }
-    
+
     public int getHora()
     {
         return hora;
     }
-    
+
     public int getMinutos()
     {
         return minutos;
